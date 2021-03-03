cf0cfd58cb7440ae5fa2cc728fb6687ed0e9f897
Joshua Gore <joshua_gore@eagle.co.nz>
2021-03-03 22:51:33 +1300

    added logo and changed app bar color

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,52 @@
 module.exports = {
-  siteMetadata: {
-    title: `Tawa Christadelphian Ecclesia`,
-    description: `For as truly as I live all the earth shall be filled with the glory of Yahweh`,
-    author: `Joshua Gore`,
-  },
-  plugins: [
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Tawa Christadelphian Ecclesia`,
+        description: `For as truly as I live all the earth shall be filled with the glory of Yahweh`,
+        author: `Joshua Gore`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `2xwhdgg5nph4`,
-        accessToken: `uCK9aJ5rf-u-FA5alwicM_xvMB3yMspsPVTJPPWqCdo`,
-      },
-    },
-  ],
-}
+    plugins: [
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-image`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-typescript`,
+            options: {
+                isTSX: true, // defaults to false
+                jsxPragma: `jsx`, // defaults to "React"
+                allExtensions: true, // defaults to false
+            },
+        },
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `2xwhdgg5nph4`,
+                accessToken: `uCK9aJ5rf-u-FA5alwicM_xvMB3yMspsPVTJPPWqCdo`,
+            },
+        },
+    ],
+};
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,30 @@ import { Link } from 'gatsby';
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import LogoBible from '../images/bible.svg';
+// color 61, 142, 255
 const Header = ({ siteTitle }: { siteTitle: string }) => (
-    <AppBar position="static">
+    <AppBar
+        position="static"
+        style={{ background: 'white', paddingTop: 20, paddingBottom: 20, paddingLeft: 20, paddingRight: 20 }}
+    >
         <Toolbar>
-            logo
-            <Typography variant="h6">{siteTitle}</Typography>
-            <IconButton edge="end">
-                <MenuIcon></MenuIcon>
-            </IconButton>
+            <img
+                src={LogoBible}
+                style={{
+                    marginRight: 40,
+                    marginTop: 0,
+                    marginBottom: 0,
+                }}
+            />
+            <Typography variant="h3" style={{ color: '#404040' }}>
+                {siteTitle}
+            </Typography>
+            <div style={{ marginLeft: 'auto', marginRight: 0 }}>
+                <IconButton edge="end">
+                    <MenuIcon style={{height: 50, width: 50}} />
+                </IconButton>
+            </div>
         </Toolbar>
     </AppBar>
 );
