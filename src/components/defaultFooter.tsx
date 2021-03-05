aec80b245a8788251fdbb344a49294bf3fca7ffd
Joshua Gore <joshua_gore@eagle.co.nz>
2021-03-06 02:05:20 +1300

    updated styling of footer

--- src/components/defaultFooter.tsx
+++ src/components/defaultFooter.tsx
@@ -1,20 +1,40 @@
-import { Typography } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 import { Link } from 'gatsby';
 import React from 'react';
 
 const DefaultFooter: React.FC = () => {
     return (
-        <div style={{ marginTop: `2rem` }}>
-            <Typography variant="h2">More Info</Typography>
-            <Link to="/">Home</Link>
-            <br />
-            <Link to="#">About Us</Link>
-            <br />
-            <Link to="#">Presentations</Link>
-            <br />
-            <Link to="/articles/">Articles</Link>
-            <br />© {new Date().getFullYear()}, Tawa Christadelphian Ecclesia
-        </div>
+        <>
+            <div
+                style={{
+                    width: '100%',
+                    backgroundColor: '#2f2e2e',
+                    color: '#a0a09f',
+                }}
+            >
+                <Container disableGutters={true} style={{}}>
+                    <div style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+                        <Typography variant="h2">More Info</Typography>
+                        <Link to="/">Home</Link>
+                        <br />
+                        <Link to="#">About Us</Link>
+                        <br />
+                        <Link to="#">Presentations</Link>
+                        <br />
+                        <Link to="/articles/">Articles</Link>
+                        <br />© {new Date().getFullYear()}, Tawa Christadelphian
+                        Ecclesia
+                    </div>
+                </Container>
+            </div>
+            <div
+                style={{
+                    width: '100%',
+                    backgroundColor: '#1b1a1a',
+                    height: '50px',
+                }}
+            ></div>
+        </>
     );
 };
 
